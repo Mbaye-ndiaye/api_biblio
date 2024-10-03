11d1a27f4b4a4054561b0d9d3b4ab55981049de9
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-10-03 12:03:35 +0000

    creer fhichier serializer

--- gestion_backend/testdb/views.py
+++ gestion_backend/testdb/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+
+@api_view(['POST'])
+def login_api(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(valid)
