9fc2768aaae5d1b0827144ddae24279faa15f30b
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-10-18 15:58:29 +0000

    deployer

--- gestion_backend/gestion_backend/settings.py
+++ gestion_backend/gestion_backend/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,6 +102,7 @@ DATABASES = {
     }
 }
 
+# postgresql://bakeli_django_render_user:zELFjo43ZORG49WVzfASHNNuk0R1LHhx@dpg-cs8j6pi3esus73aqotfg-a.oregon-postgres.render.com/bakeli_django_render
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -137,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
