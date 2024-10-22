2bd8a8078b5e761fed026af69be48f73ad0bb916
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-10-22 09:07:19 +0000

    new

--- gestion_backend/gestion_backend/settings.py
+++ gestion_backend/gestion_backend/settings.py
@@ -27,11 +27,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
