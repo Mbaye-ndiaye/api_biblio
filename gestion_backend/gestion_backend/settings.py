43466527ef21d60af2056dd17a739c7a4d14435d
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-10-22 10:22:46 +0000

    new

--- gestion_backend/gestion_backend/settings.py
+++ gestion_backend/gestion_backend/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-14&=44w7lzf)u5%q$z_&pc-zf5@f^2*=c79^4cq@em9=9@d^kf')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
