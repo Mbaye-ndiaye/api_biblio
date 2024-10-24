57c427738700e790c97ccc937c48618b4ba2b142
CheikhDev <cheikhdev99@gmail.com>
2024-10-24 15:08:27 +0000

    1st Commit du 24/10/2024

--- gestion_backend/testdb/serializers/emprunt_serializer.py
+++ gestion_backend/testdb/serializers/emprunt_serializer.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 class EmpruntSerializer(serializers.ModelSerializer):
     livre_nom = serializers.CharField(source='livre.title', read_only=True)
-    membre_nom = serializers.CharField(source='membre.nom', read_only=True)
+    membre_nom = serializers.CharField(source='membre.nom', read_only=True) 
 
     class Meta:
         model = Emprunt
@@ -18,18 +18,19 @@ class EmpruntSerializer(serializers.ModelSerializer):
         if livre.available_copies <= 0:
             raise serializers.ValidationError("Aucune copie disponible pour ce livre.")
 
+        # Mise à jour des copies disponibles du livre
         livre.available_copies -= 1
         livre.save()
 
         membre = validated_data['membre']
-    # Accéder au modèle Member si nécessaire
+        
+        # Récupérer le prénom et le nom du membre
         try:
-           membre_instance = Member.objects.get(user=membre)
-           validated_data['membre_nom'] = membre_instance.nom + " " + membre_instance.prenom
+            membre_instance = Member.objects.get(user=membre)
+            validated_data['membre_nom'] = f"{membre_instance.prenom} {membre_instance.nom}"
         except Member.DoesNotExist:
             raise serializers.ValidationError("Membre introuvable.")
 
         validated_data['livre_nom'] = livre.title
 
         return super().create(validated_data)
-
