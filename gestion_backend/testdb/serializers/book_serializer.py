a37415a2cf2498053fe78e4b99b63dd620c0c28c
User <bam480836@gmail.com>
2024-11-04 14:41:09 +0000

    ba

--- gestion_backend/testdb/serializers/book_serializer.py
+++ gestion_backend/testdb/serializers/book_serializer.py
@@ -6,6 +6,14 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+        
+    def validate_cover_image(self, value):
+        if value:
+            if value.content_type not in ['image/jpeg', 'image/png']:
+                raise serializers.ValidationError("Le fichier n'est pas une image valide.")
+        else:
+            raise serializers.ValidationError("Aucun fichier téléchargé.")
+        return value
     
     def validate_available_copies(self, value):
         # Vérifiez si l'instance est définie (pour les mises à jour)
