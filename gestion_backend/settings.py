ff0501812ba2fddb1f087f7d07ad54de1e8be898
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-11-12 10:18:32 +0000

    ajouter dans le render

--- gestion_backend/settings.py
+++ gestion_backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "testdb",
+    'testdb',
     'rest_framework',
     'knox',
     'rest_framework_simplejwt.token_blacklist',
