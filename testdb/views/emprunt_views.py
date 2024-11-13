768bf015d128609746c12359b41d566cce184ed6
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-11-13 15:49:15 +0000

    push

--- testdb/views/emprunt_views.py
+++ testdb/views/emprunt_views.py
@@ -237,3 +237,50 @@ class EmpruntListCreateView(APIView):
             serializer.save(membre=request.user)  # Utilisation de l'utilisateur connecté
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmpruntDetailView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Emprunt.objects.get(pk=pk)
+        except Emprunt.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        emprunt = self.get_object(pk)
+        serializer = EmpruntSerializer(emprunt)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        emprunt = self.get_object(pk)
+
+        if emprunt.rendu:
+            return Response({"error": "Ce livre a déjà été rendu."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Marquer l'emprunt comme rendu
+        emprunt.rendu = True
+        emprunt.date_retour = timezone.now()
+
+        # Incrémenter le nombre de copies disponibles du livre
+        emprunt.livre.available_copies += 1
+        emprunt.livre.save()
+
+        emprunt.save()
+
+        serializer = EmpruntSerializer(emprunt)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        emprunt = self.get_object(pk)
+
+        if emprunt.rendu is False:
+            # Si l'emprunt n'est pas rendu, remettre le livre en stock avant suppression
+            emprunt.livre.available_copies += 1
+            emprunt.livre.save()
+
+        emprunt.delete()
+        return Response({"message": "Emprunt supprimé avec succès."}, status=status.HTTP_204_NO_CONTENT)
+
+
