6ac3f6e013d4adb504a11b3eee91a4f4dd86037e
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-04 13:56:12 +0000

    teste

--- testdb/urls.py
+++ testdb/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login, logout, get_users, MemberListCreateView, MemberDetailView, EmpruntListCreateView, EmpruntDetailView
+from .views import register, login, logout, get_users, EmpruntListCreateView, EmpruntDetailView
 from .views import BookListCreateView, BookDetailView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf.urls.static import static
@@ -12,8 +12,8 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     
     # Endpoints pour les membres
-    path('membres/', MemberListCreateView.as_view(), name='member-list-create'),
-    path('membres/<int:pk>/', MemberDetailView.as_view(), name='member-detail'),
+    # path('membres/', MemberListCreateView.as_view(), name='member-list-create'),
+    # path('membres/<int:pk>/', MemberDetailView.as_view(), name='member-detail'),
 
     # Endpoints pour les emprunts
     path('emprunts/', EmpruntListCreateView.as_view(), name='emprunt-list-create'),
